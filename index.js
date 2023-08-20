4cbb6e08334354d20f04c8b47c96383ed3007c1c
Aditya Bhatt <bhattaditya92@gmail.com>
2023-08-20 18:01:26 +0530

    yarn and cors updated

--- index.js
+++ index.js
@@ -26,8 +26,13 @@ mongoose.connect(
   console.log(err);
 });
 
+const corsOptions = {
+  origin: process.env.CLIENT ||'http://localhost:3000',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
 app.use(limiter);
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json())
 app.use("/api/auth" , authRoute);
 app.use("/api/users" , userRoute);
